fba681d346643d6b94ef1fb2407fb53b18a1704a
mraihaan22 <mraihaan64@gmail.com>
2024-12-27 20:49:37 +0700

    Sc Wa

---
+++ config.js
@@ -0,0 +1,48 @@
+/*
+  >> Control <<
+*/
+global.owner = ['6283809358811']
+global.ownername = "EhaN"
+global.connect = true // ubah ke false klo mau connect lewat qrcode 
+global.antilink = false
+global.autotyping = false
+
+/*
+  
+  >> Payment <<
+*/
+global.dana = "XXXX"
+global.gopay = "XXXX"
+global.ovo = "XXXX"
+global.qris = "https://files.catbox.moe/5dfafw.jpg"
+
+/*
+  >> Message <<
+*/
+global.mess = {
+"ketua": " ⇝ Access Denied \nKhusus Owner",
+"prem": "⇝ Access Denied \nBeli Akses Premium Di Owner",
+"premium": "⇝ Access Denied \nBeli Akses Premium Di Owner",
+"japost": " -- Format Japost Tidak Tersedia -- ",
+"rekber": " -- List Rekber Tidak Tersedia -- "
+}
+
+/*
+  >> MISC <<
+*/
+global.tele = "t.me/"
+global.tele2 = "t.me/"
+global.waMe = "wa.me/6283809358811"
+global.tutorialBot = "https://youtube.com" // Untuk menggampangkan buyer awam saat beli panel 
+global.versionofscript = "8.0"
+global.url = "https://files.catbox.moe/i7jxyz.jpg" // buat banner
+global.urlbanner = "https://files.catbox.moe/zay0u6.jpg" // buat banner 2
+global.url2 = "https://youtube.com" // isi url bebas buat reply
+global.packname = "Cihuy" // sticker
+global.author = "Anjays" // sticker
+global.group = "https://whatsapp.com" // isi group bebas lu
+global.idCH = "120363333509194874@newsletter"
+global.xchannel = {
+	jid: '120363333509194874@newsletter'
+	}
+	
