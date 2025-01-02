ea6242f1a98ea145c15e875ab87b91a86de98415
ryzens65 <mraihaan64@gmail.com>
2025-01-02 18:55:59 +0700

    uhh

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "7440325340:AAGXTCGCkS90mV-2ltF9rCWGloqWX2isrzA", // Token bot Telegram
+  OWNER_ID: "6111586845", // ID pemilik bot
+  allowedGroupIds: [], // ID grup yang diizinkan
+};
