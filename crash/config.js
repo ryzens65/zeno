9b43464325dd3ed6797b5227dcc277d06de64cc7
ryzens65 <mraihaan64@gmail.com>
2025-01-06 20:20:04 +0700

    newer

--- crash/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  BOT_TOKEN: "7440325340:AAGXTCGCkS90mV-2ltF9rCWGloqWX2isrzA",
-  OWNER_ID: "6111586845",
-  allowedGroupIds: [],
-};
---
+++ crash/undefined.js
@@ -0,0 +1,11 @@
+const global = {
+    superAdmin: [6111586845], // id mu
+    superUser: [6111586845], // id mu
+    usernameOwner: "@permen_md",
+    botName: "𝖚𝖓𝖉𝖊𝖋𝖎𝖓𝖊𝖉 𝖈𝖗𝖚𝖓𝖈𝖍𝖊𝖗",
+    botToken: "7440325340:AAGXTCGCkS90mV-2ltF9rCWGloqWX2isrzA",
+    channelLink: "https://t.me/NullxTeam",
+    preview: 'https://j.top4top.io/m_32448eu0w1.mp4'
+}
+
+module.exports = global;
